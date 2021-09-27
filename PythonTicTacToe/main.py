f783ad8b9d9d2aaa703de34754955216020f343c
Amin <76617044+AminScript@users.noreply.github.com>
2021-09-27 12:30:20 +0330

    debug

--- PythonTicTacToe/main.py
+++ PythonTicTacToe/main.py
@@ -1,23 +1,67 @@
-def binary_search(l, key):
-    first = 0
-    end = len(l) - 1
-    while first <= end:
-        middle = (first + end) // 2
-        if l[middle] < key:
-            first = middle + 1
-        elif l[middle] > key:
-            end = middle - 1
+array2d = []
+
+for i in range(3):
+    new_line = []
+    for j in range(3):
+        new_line.append("-")
+    array2d.append(new_line)
+
+
+def show():
+    for i in array2d:
+        for j in i:
+            print(j, end=' ')
+        print()
+
+def win():
+    for i in range(3):
+        if array2d[0][i] == array2d[1][i] == array2d[2][i]:
+            if array2d[0][i] == 'x':
+                print("X Bord")
+                break
+            elif array2d[0][i] == 'y':
+                print("Y Bord")
+                break
+
+    for i in range(3):
+        if array2d[i][0] == array2d[i][1] == array2d[i][2]:
+            if array2d[i][0] == 'x':
+                print("X Bord")
+                break
+            elif array2d[i][0] == 'y':
+                print("Y Bord")
+                break
+
+    if array2d[0][0] == array2d[1][1] and array2d[0][0] == array2d[2][2] and array2d[0][0] != "-":
+        if array2d[0][0] == 'x':
+            print("X Bord")
         else:
-            return middle
-    return -1
+            print("Y Bord")
+
+    if array2d[0][2] == array2d[1][1] and array2d[0][2] == array2d[2][0] and array2d[0][2] != "-":
+        if array2d[0][2] == 'x':
+            print("X Bord")
+        else:
+            print("Y Bord")
+
+
+show()
+turn = 'x'
+while True:
+    print("Enter Location")
+    a = int(input())
+    b = int(input())
+
+    if array2d[a][b] == "-":
+        array2d[a][b] = turn
+    show()
+    win()
+
+    if turn == 'x':
+        turn = 'y'
+    else:
+        turn = 'x'
+    # turn = 'x' if turn == 'y' else 'y'
 
 
 
-
-li = [1, 2, 4, 5]
-result = binary_search(li, 2)
-
-if result == -1:
-    print("Not Found")
-else:
-    print("Found" + str(result))
